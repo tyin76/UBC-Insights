d0ecef76615cf72874bd7fdde1b0a9646fbe9fbe
Jake Yeo <jyeo03@students.cs.ubc.ca>
2024-10-27 17:46:58 -0700

    Added listDataset and addDataset tests

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -68,6 +68,23 @@ describe("InsightFacade", function () {
 			}
 		});
 
+		it("should list out all datasets with correct ids when adding both sections and rooms [listDatasets]", async function () {
+			try {
+				const validRooms = await getContentFromArchives("campus.zip");
+
+				await facade.addDataset("rooms", validRooms, InsightDatasetKind.Sections);
+
+				await facade.addDataset("sections", sections, InsightDatasetKind.Sections);
+				const datasetArray = await facade.listDatasets();
+
+				expect(datasetArray[0].id).be.equal("rooms");
+				expect(datasetArray[1].id).be.equal("sections");
+			} catch (err) {
+				const errorMessage = (err as Error).message;
+				expect.fail(`The test should not reach the catch block. Error: ${errorMessage}`);
+			}
+		});
+
 		it("should list out all datasets with correct ids with different instance of facade [listDatasets]", async function () {
 			try {
 				await facade.addDataset("ubc1", sections, InsightDatasetKind.Sections);
@@ -112,6 +129,88 @@ describe("InsightFacade", function () {
 
 				expect(datasetArray[0].numRows).to.equal(rowsInDataset);
 				expect(datasetArray[1].numRows).to.equal(rowsInDataset);
+			} catch (err) {
+				const errorMessage = (err as Error).message;
+				expect.fail(`The test should not reach the catch block. Error: ${errorMessage}`);
+			}
+		});
+
+		it("should list out all section datasets with correct row after adding room datasets [listDatasets]", async function () {
+			try {
+				const validRooms = await getContentFromArchives("campus.zip");
+
+				const rowsInDataset = 18;
+
+				await facade.addDataset("sections", sections, InsightDatasetKind.Sections);
+
+				await facade.addDataset("rooms", validRooms, InsightDatasetKind.Rooms);
+
+				await facade.addDataset("sections2", sections, InsightDatasetKind.Sections);
+
+				const datasetArray = await facade.listDatasets();
+
+				const positionOfSections2 = 2;
+
+				expect(datasetArray[0].numRows).to.equal(rowsInDataset);
+				expect(datasetArray[positionOfSections2].numRows).to.equal(rowsInDataset);
+			} catch (err) {
+				const errorMessage = (err as Error).message;
+				expect.fail(`The test should not reach the catch block. Error: ${errorMessage}`);
+			}
+		});
+
+		it("should list out all added section and room datasets with correct ids with different instance of facade [listDatasets]", async function () {
+			try {
+				const validRooms = await getContentFromArchives("campus.zip");
+
+				await facade.addDataset("sections", sections, InsightDatasetKind.Sections);
+
+				await facade.addDataset("rooms", validRooms, InsightDatasetKind.Rooms);
+
+				const newFacade = new InsightFacade();
+
+				const datasetArray = await newFacade.listDatasets();
+
+				expect(datasetArray[0].id).be.equal("sections");
+				expect(datasetArray[1].id).be.equal("rooms");
+			} catch (err) {
+				const errorMessage = (err as Error).message;
+				expect.fail(`The test should not reach the catch block. Error: ${errorMessage}`);
+			}
+		});
+
+		it("should list out all datasets with correct kinds when adding room and section datasets [listDatasets]", async function () {
+			try {
+				const validRooms = await getContentFromArchives("campus.zip");
+
+				await facade.addDataset("sections", sections, InsightDatasetKind.Sections);
+
+				await facade.addDataset("rooms", validRooms, InsightDatasetKind.Rooms);
+
+				const datasetArray = await facade.listDatasets();
+
+				expect(datasetArray[0].kind).to.equal(InsightDatasetKind.Sections);
+				expect(datasetArray[1].kind).to.equal(InsightDatasetKind.Rooms);
+			} catch (err) {
+				const errorMessage = (err as Error).message;
+				expect.fail(`The test should not reach the catch block. Error: ${errorMessage}`);
+			}
+		});
+
+		it("should list out all datasets with correct kinds when using a new facade when adding room and section datasets [listDatasets]", async function () {
+			try {
+				const validRooms = await getContentFromArchives("campus.zip");
+
+				await facade.addDataset("sections", sections, InsightDatasetKind.Sections);
+
+				await facade.addDataset("rooms", validRooms, InsightDatasetKind.Rooms);
+
+				const newFacade = new InsightFacade();
+
+				const datasetArray = await newFacade.listDatasets();
+
+				expect(datasetArray[0].kind).to.equal(InsightDatasetKind.Sections);
+				expect(datasetArray[1].kind).to.equal(InsightDatasetKind.Rooms);
 			} catch (err) {
 				const errorMessage = (err as Error).message;
 				expect.fail(`The test should not reach the catch block. Error: ${errorMessage}`);
