9288d1dff48e3b4835ad894dac6db8255c16f7a7
terenceyin <tyin05@student.ubc.ca>
2024-11-19 18:49:53 -0800

    added more tests to spec.ts

--- test/rest/Server.spec.ts
+++ test/rest/Server.spec.ts
@@ -56,5 +56,51 @@ describe("Facade C3", function () {
 		}
 	});
 
+	it("PUT test for adding invalid sections dataset", async function () {
+		const SERVER_URL = "http://localhost:4321";
+		const id = "valid123";
+		const kind = InsightDatasetKind.Sections;
+		const ENDPOINT_URL = `/dataset/${id}/${kind}`;
+		const ZIP_FILE_DATA = await getContent("invalidSectionSmall.zip");
+
+		try {
+			return request(SERVER_URL)
+				.put(ENDPOINT_URL)
+				.send(ZIP_FILE_DATA)
+				.set("Content-Type", "application/x-zip-compressed")
+				.then(function (res: Response) {
+					// some logging here please!
+					expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
+				});
+		} catch (err) {
+			Log.error(err);
+			expect.fail();
+			// and some more logging here!
+		}
+	});
+
+	it("PUT test for adding valid sections dataset with invalid ID", async function () {
+		const SERVER_URL = "http://localhost:4321";
+		const id = " ";
+		const kind = InsightDatasetKind.Sections;
+		const ENDPOINT_URL = `/dataset/${id}/${kind}`;
+		const ZIP_FILE_DATA = await getContent("validSmall.zip");
+
+		try {
+			return request(SERVER_URL)
+				.put(ENDPOINT_URL)
+				.send(ZIP_FILE_DATA)
+				.set("Content-Type", "application/x-zip-compressed")
+				.then(function (res: Response) {
+					// some logging here please!
+					expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
+				});
+		} catch (err) {
+			Log.error(err);
+			expect.fail();
+			// and some more logging here!
+		}
+	});
+
 	// The other endpoints work similarly. You should be able to find all instructions in the supertest documentation
 });
