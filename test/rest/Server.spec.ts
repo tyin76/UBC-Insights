c59c7daef4a733681c131540b4b89f7b6de8026b
terenceyin <tyin05@student.ubc.ca>
2024-11-19 17:53:03 -0800

    back to original server.spec

--- test/rest/Server.spec.ts
+++ test/rest/Server.spec.ts
@@ -2,53 +2,29 @@ import { expect } from "chai";
 import request, { Response } from "supertest";
 import { StatusCodes } from "http-status-codes";
 import Log from "@ubccpsc310/folder-test/build/Log";
-import Server from "../../src/rest/Server"; // Adjust path based on your project structure
-import { getContentFromArchives } from "../TestUtil";
-import { InsightDatasetKind } from "../../src/controller/IInsightFacade";
 
 describe("Facade C3", function () {
-	let serverInstance: Server;
-	let validZipFile: string;
-	const SERVER_URL = "http://localhost:4321"; // Replace with your actual server URL
-
-	before(async function () {
-		try {
-			Log.info("Starting server...");
-			validZipFile = await getContentFromArchives("pair.zip"); // getContentFromArchives already returns to base64
-			//console.log(typeof validZipFile);
-			serverInstance = new Server(4321); // Initialize the server instance with the appropriate port
-			await serverInstance.start(); // Start the server
-			Log.info("Server started successfully.");
-		} catch (error) {
-			Log.error("Error starting server:", error);
-			throw error; // Fail the tests if the server cannot start
-		}
+	before(function () {
+		// TODO: start server here once and handle errors properly
 	});
 
-	after(async function () {
-		try {
-			Log.info("Stopping server...");
-			await serverInstance.stop(); // Stop the server gracefully
-			Log.info("Server stopped successfully.");
-		} catch (error) {
-			Log.error("Error stopping server:", error);
-		}
+	after(function () {
+		// TODO: stop server here once!
 	});
 
 	beforeEach(function () {
-		Log.info("Starting a new test...");
+		// might want to add some process logging here to keep track of what is going on
 	});
 
 	afterEach(function () {
-		Log.info("Finished a test.");
+		// might want to add some process logging here to keep track of what is going on
 	});
 
 	// Sample on how to format PUT requests
 	it("PUT test for courses dataset", function () {
-		const id = "validID"; // Specify actual dataset ID
-		const kind = InsightDatasetKind.Sections; // Specify actual kind
-		const ENDPOINT_URL = `/dataset/${id}/${kind}`;
-		const ZIP_FILE_DATA = validZipFile;
+		const SERVER_URL = "TBD";
+		const ENDPOINT_URL = "TBD";
+		const ZIP_FILE_DATA = "TBD";
 
 		try {
 			return request(SERVER_URL)
